016f8675cb94efc576df75b1f1d0628953f06118
JimSeker <seker@uwyo.edu>
2017-08-24 14:41:28 -0600

    major rework for API 26
    far better example now too.

--- BroadCastDemo2/app/src/main/java/edu/cs4730/broadcastdemo2/MainFragment.java
+++ BroadCastDemo2/app/src/main/java/edu/cs4730/broadcastdemo2/MainFragment.java
@@ -1,67 +1,107 @@
 package edu.cs4730.broadcastdemo2;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 /**
- *  this app will need to be run on emulator to test the varying pieces
- *  unless you can charge and discharge your battery very quickly.
- *  but on a device plugin it in/unplug it to see no charging messages.
+ * this app will need to be run on emulator to test the varying pieces
+ * unless you can charge and discharge your battery very quickly.
+ * but on a device plugin it in/unplug it to see no charging messages.
+ * <p>
+ * In AIP 25 and below, the broadcast receiver could be static declared in the manifest.xml file
+ * but in API 26+ the implicit broadcast are no longer allowed.
  */
 public class MainFragment extends Fragment {
 
-    int mStatus = 0;
     TextView logger;
-    
-	public MainFragment() {
-		// Required empty public constructor
-	}
+    MyReceiver mReceiver = new MyReceiver();
+    String TAG = "MainFragment";
 
-    public MainFragment(int status) {
-		mStatus = status;
-	}
-	
-	
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		// Inflate the layout for this fragment
-		View myView = inflater.inflate(R.layout.fragment_main, container, false);
-		logger = (TextView) myView.findViewById(R.id.textView1);
-		setstat(mStatus);
-		return myView;
-	}
+    public MainFragment() {
+        // Required empty public constructor
+    }
 
-	public void setstat( int s) {
-		mStatus = s;
-		String info = "";
-		switch (s) {
-		case 0:
-			info= "\n should not get a zero, unless app just started";
-			break;
-		case 1:
-			info= "\n batter low. should shut down things.";
-			break;
-		case 2:
-			info= "\n battery Okay.  ";
-			break;
-		case 3:
-			info= "\n Power connected, so phone is charging.";
-			break;
-		case 4:
-			info= "\n Power disconnected.";
-			break;
-		default:
-			info= "\n something wrong.";
-				
-		} 
-		logger.setText("status is " + mStatus+ info);
-		
-		
-	}
-	
+    /* register the receiver (declared below) to get the information charging and batter */
+    @Override
+    public void onResume() {
+        super.onResume();
+        //the one below registers a global receiver.
+        IntentFilter myIF = new IntentFilter("android.intent.action.ACTION_POWER_CONNECTED");
+        myIF.addAction("android.intent.action.ACTION_POWER_DISCONNECTED");
+        myIF.addAction("android.intent.action.BATTERY_LOW");
+        myIF.addAction("android.intent.action.BATTERY_OKAY");
+        getActivity().registerReceiver(mReceiver, myIF);
+        Log.v(TAG, "receiver should be registered");
+    }
+
+    @Override
+    public void onPause() {  //or onDestory()
+        // Unregister since the activity is not visible.
+
+        getActivity().unregisterReceiver(mReceiver);
+        Log.v(TAG, "receiver should be unregistered");
+        super.onPause();
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View myView = inflater.inflate(R.layout.fragment_main, container, false);
+        logger = myView.findViewById(R.id.textView1);
+        return myView;
+    }
+
+
+	/*
+     * To test this on the emulator, remember to telnet to the emulator
+    * commands:
+    * power ac off      phone is unplugged
+    * power ac on       phone is plugged in/charging
+    * power capacity  15    low (when unplugged)
+    * power capacity 30     okay.. (when plugged in and was low)
+    * power capacity 100   full charge.
+    */
+
+    public class MyReceiver extends BroadcastReceiver {
+        public MyReceiver() {
+        }
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            Log.v("myReceiver", "received an intent");
+            String info = "\n something wrong.";
+            int mStatus = 0;
+            if (intent.getAction().equals(Intent.ACTION_BATTERY_LOW)) {  //battery is low!
+                info = "\n batter low. should shut down things.";
+                mStatus = 1;
+                Log.v("myReceiver", "battery low");
+            } else if (intent.getAction().equals(Intent.ACTION_BATTERY_OKAY)) {  //battery is now ok!
+                info = "\n battery Okay.  ";
+                mStatus = 2;
+                Log.v("myReceiver", "battery okay");
+            } else if (intent.getAction().equals(Intent.ACTION_POWER_CONNECTED)) {  //battery is charging.
+                info = "\n Power connected, so phone is charging.";
+                mStatus = 3;
+                Log.v("myReceiver", "ac on");
+            } else if (intent.getAction().equals(Intent.ACTION_POWER_DISCONNECTED)) {  //power has been disconnected.
+                info = "\n Power disconnected.";
+                mStatus = 4;
+                Log.v("myReceiver", "ac off");
+            }
+            logger.setText("status is " + mStatus + info);
+        }
+    }
+
 }
