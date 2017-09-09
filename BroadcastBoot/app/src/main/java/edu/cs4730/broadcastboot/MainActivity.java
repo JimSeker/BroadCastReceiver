a6a1962efa73dfa7c51f39c7d5eaa09b40bd52d1
JimSeker <seker@uwyo.edu>
2017-09-08 21:30:54 -0600

    fixed a dumb error.

--- BroadcastBoot/app/src/main/java/edu/cs4730/broadcastboot/MainActivity.java
+++ BroadcastBoot/app/src/main/java/edu/cs4730/broadcastboot/MainActivity.java
@@ -41,6 +41,9 @@ public class MainActivity extends AppCompatActivity {
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, mFragment).commit();
+        } else {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, mFragment).commit();
         }
 
         createchannel();
